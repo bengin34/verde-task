ceafee6481fba0995b9255cae01f9450b4eacf08
bengin34 <becaglar3434@gmail.com>
2023-02-28 10:53:14 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,110 @@
+import React, { useState } from "react";
+import { BsFillBellFill } from "react-icons/bs";
+import { RiApps2Fill } from "react-icons/ri";
+
+const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+  return (
+    <div className="dark">
+      <nav className=" bg-white border-gray-200 px-2 sm:px-4 py-2.5  dark:bg-gray-900">
+        <div className="flex flex-wrap items-center justify-between mx-auto">
+          <a href="https://flowbite.com/" className="flex items-center">
+            <img
+              src="https://flowbite.com/docs/images/logo.svg"
+              className="h-6 mr-3 sm:h-9"
+              alt="Flowbite Logo"
+            />
+            <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+              Arbit Blog
+            </span>
+          </a>
+
+          <button
+            data-collapse-toggle="navbar-default"
+            type="button"
+            className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            aria-controls="navbar-default"
+            aria-expanded={isMenuOpen}
+            onClick={toggleMenu}
+          >
+            <span className="sr-only">Open main menu</span>
+            <svg
+              className="w-6 h-6"
+              aria-hidden="true"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+          </button>
+          <div
+            className={`w-full md:block md:w-auto ${
+              isMenuOpen ? "block" : "hidden"
+            } `}
+            id="navbar-default"
+          >
+            <ul className="flex flex-col items-center p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+              <li>
+                <a
+                  href=""
+                  className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent relative"
+                  aria-current="page"
+                >
+                  Posts
+                  <div
+                    className="rounded-full bg-slate-400 flex justify-center items-center"
+                    style={{
+                      color: "white",
+                      width: "1.125rem",
+                      height: "1.125rem",
+                      position: "absolute",
+                      top: "-10px",
+                      right: "-18px",
+                    }}
+                  >
+                    {" "}
+                    2{" "}
+                  </div>
+                </a>
+              </li>
+              <li>
+                <a
+                  href=""
+                  className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                >
+                  <BsFillBellFill />
+                </a>
+              </li>
+              <li>
+                <a
+                  href=""
+                  className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                >
+                  <RiApps2Fill />
+                </a>
+              </li>
+              <li>
+                <img
+                  className="w-10 h-10 p-1 rounded-full ring-2 ring-gray-300 dark:ring-gray-500"
+                  src="https://xsgames.co/randomusers/assets/avatars/male/63.jpg"
+                  alt="Bordered avatar"
+                />
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
